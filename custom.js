84656aa503cbf1e54d9de012123cdd66d1c342f1
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-08 19:19:45 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.getJSON("quotes_final.json", function(json) {
         let quoteIndex = Math.floor(Math.random() * (json.length));
-            console.log(json.length); 
+            console.log(json[author]); 
             console.log(quoteIndex);
         })
 });
