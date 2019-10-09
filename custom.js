60419ba0ae1fdae1986dc635cd123e9ffef91384
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-09 15:49:43 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.getJSON("quotes_final.json", function(json) {
         const quoteData = json;
-        console.log(json);
+        console.log(quoteData);
         let quoteIndex = Math.floor(Math.random() * (json.length));
 
         $(".full-quote").html(json[quoteIndex].quote.beginningQuote);
