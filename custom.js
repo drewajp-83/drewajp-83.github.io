4ac977c3be0c437adb868913ebbcb350e2b1b921
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-09 19:45:23 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -8,10 +8,19 @@
           let middleQuoteIndex = Math.floor(Math.random() * (json.length));
           let endQuoteIndex = Math.floor(Math.random() * (json.length));
 
-          //output
+          //output (for quote testing)
           $(".full-quote").html(json[quoteIndex].quote.fullQuote);
           $(".author").html(json[quoteIndex].author);
           $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
               json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
+
+          //create dropdown for fullquote or mixed quote selection. Wrap outputs for each instance in functions and call on selection
+
+          //create dropdown for 1 - 5 quote selection to generate <li> attributes and loop through on number input
+
+          //event listener to denerate quotes
+
+          //event listener to clear quotes
+
       })
   });
