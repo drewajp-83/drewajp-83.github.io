9f184fe46b360ff2d82976197eaa0a875304c4be
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-09 16:59:54 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
     $.getJSON("quotes_final.json", function(json) {
         //obtains array value to generate random full quote
         let quoteIndex = Math.floor(Math.random() * (json.length));
-
+        
+        //output
         $(".full-quote").html(json[quoteIndex].quote.fullQuote);
         $(".author").html(json[quoteIndex].author);
     })
