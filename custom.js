8ed66fea8c87b0923d5ef364f677d5422620a219
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-09 18:13:55 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,19 +1,21 @@
-$(document).ready(function() {
-    $.getJSON("quotes_final.json", function(json) {
-        //obtains random array number to generate random full quote
-        let quoteIndex = Math.floor(Math.random() * (json.length));
+  $(document).ready(function() {
+      $.getJSON("quotes_final.json", function(json) {
+          //obtains random array number to generate random full quote
+          let quoteIndex = Math.floor(Math.random() * (json.length));
 
-        //obtains random array number for each quote fragment
-        let beginningQuoteIndex = Math.floor(Math.random() * (json.length));
-        let middleQuoteIndex = Math.floor(Math.random() * (json.length));
-        let endQuoteIndex = Math.floor(Math.random() * (json.length));
-        console.log(quoteIndex);
-        console.log(beginningQuoteIndex);
-        console.log(middleQuoteIndex);
-        console.log(endQuoteIndex);
-        
-        //output
-        $(".full-quote").html(json[quoteIndex].quote.fullQuote);
-        $(".author").html(json[quoteIndex].author);
-    })
-});
+          //obtains random array number for each quote fragment
+          let beginningQuoteIndex = Math.floor(Math.random() * (json.length));
+          let middleQuoteIndex = Math.floor(Math.random() * (json.length));
+          let endQuoteIndex = Math.floor(Math.random() * (json.length));
+          console.log(quoteIndex);
+          console.log(beginningQuoteIndex);
+          console.log(middleQuoteIndex);
+          console.log(endQuoteIndex);
+
+          //output
+          $(".full-quote").html(json[quoteIndex].quote.fullQuote);
+          $(".author").html(json[quoteIndex].author);
+          $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
+              json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote)
+      })
+  });
