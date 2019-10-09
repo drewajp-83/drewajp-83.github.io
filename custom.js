dcb08916920eec0230022c086de69f875f0409f0
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-09 17:23:02 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
     $.getJSON("quotes_final.json", function(json) {
         //obtains array value to generate random full quote
         let quoteIndex = Math.floor(Math.random() * (json.length));
-        
+
         //loop through object to get nested values
-        let nestedValues = ["quote.beginningQuote", "quote.middleQuote", "quote.endQuote"];
+        let nestedValues = json;
 
         for (let i = 0; i < nestedValues.length; i++) {
             console.log(nestedValues[i]);
         }
-        
+
         //output
         $(".full-quote").html(json[quoteIndex].quote.fullQuote);
         $(".author").html(json[quoteIndex].author);
