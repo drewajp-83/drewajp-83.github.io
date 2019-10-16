38566ed86acda9221ad00db43b251f5b5c766409
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-16 18:09:09 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,32 +1,29 @@
-  $(document).ready(function() {
-      $.getJSON("quotes_final.json", function(json) {
-        
-              //global
-              let fullQuote = "";
-              let fragmentedQuote = "";
-              let author = "";
-          
-              //obtains random array number to generate random full quote
-              const quoteIndex = Math.floor(Math.random() * (json.length));
-              
-              //obtains random array number for each quote fragment
-              const beginningQuoteIndex = Math.floor(Math.random() * (json.length));
-              const middleQuoteIndex = Math.floor(Math.random() * (json.length));
-              const endQuoteIndex = Math.floor(Math.random() * (json.length));
-        
-          //output
-              $(".full-quote").html(json[quoteIndex].quote.fullQuote);
-              $(".author").html(json[quoteIndex].author);
-              $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
-              json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
-        
-          //create dropdown for fullquote or mixed quote selection. Wrap outputs for each instance in functions and call on selection
+$(document).ready(function() {
+    $.getJSON("quotes_final.json", function(json) {
+        //global variables
+        let fullQuote = "";
+        let author = "";
+        let fragmentedQuote = "";
+        //obtains random array number to generate random full quote
+        const quoteIndex = Math.floor(Math.random() * (json.length));
+        //obtains random array number for each quote fragment
+        const beginningQuoteIndex = Math.floor(Math.random() * (json.length));
+        const middleQuoteIndex = Math.floor(Math.random() * (json.length));
+        const endQuoteIndex = Math.floor(Math.random() * (json.length));
 
-          //create dropdown for 1 - 5 quote selection to generate <li> attributes and loop through on number input
+        //output
+        $(".full-quote").html(json[quoteIndex].quote.fullQuote);
+        $(".author").html(json[quoteIndex].author);
+        $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
+            json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
 
-          //event listener to generate quotes
+        //create dropdown for fullquote or mixed quote selection. Wrap outputs for each instance in functions and call on selection
 
-          //event listener to clear quotes
+        //create dropdown for 1 - 5 quote selection to generate <li> attributes and loop through on number input
 
-      })
-  });
+        //event listener to generate quotes
+
+        //event listener to clear quotes
+
+    })
+});
