bdd228ddcabdf77326d57e18fecdf38a2444d9a8
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-16 17:19:34 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,21 +1,21 @@
   $(document).ready(function() {
       $.getJSON("quotes_final.json", function(json) {
-          function completeQuote() {
               //obtains random array number to generate random full quote
               const quoteIndex = Math.floor(Math.random() * (json.length));
-              $(".full-quote").html(json[quoteIndex].quote.fullQuote);
-              $(".author").html(json[quoteIndex].author);
-          };
-
-          function randomQuote() {
+              
               //obtains random array number for each quote fragment
               const beginningQuoteIndex = Math.floor(Math.random() * (json.length));
               const middleQuoteIndex = Math.floor(Math.random() * (json.length));
               const endQuoteIndex = Math.floor(Math.random() * (json.length));
               $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
               json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
-          };
-
+        
+          //output
+              $(".full-quote").html(json[quoteIndex].quote.fullQuote);
+              $(".author").html(json[quoteIndex].author);
+              $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
+              json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
+        
           //create dropdown for fullquote or mixed quote selection. Wrap outputs for each instance in functions and call on selection
 
           //create dropdown for 1 - 5 quote selection to generate <li> attributes and loop through on number input
