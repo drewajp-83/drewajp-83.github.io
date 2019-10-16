154c9c751f5828eb2c0e4debef4e0ccbdb9500bb
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-16 18:19:34 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
+    //global variables
+    let fullQuote = "";
+    let author = "";
+    let fragmentedQuote = "";
     $.getJSON("quotes_final.json", function(json) {
-        //global variables
-        let fullQuote = "";
-        let author = "";
-        let fragmentedQuote = "";
         //obtains random array number to generate random full quote
         const quoteIndex = Math.floor(Math.random() * (json.length));
         //obtains random array number for each quote fragment
         const beginningQuoteIndex = Math.floor(Math.random() * (json.length));
         const middleQuoteIndex = Math.floor(Math.random() * (json.length));
         const endQuoteIndex = Math.floor(Math.random() * (json.length));
-
-        //output
-        $(".full-quote").html(json[quoteIndex].quote.fullQuote);
-        $(".author").html(json[quoteIndex].author);
-        $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
-            json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
+            
+        fullQuote = $(".full-quote").html(json[quoteIndex].quote.fullQuote);
+        author = $(".author").html(json[quoteIndex].author);
+        
+        fragmentedQuote = $(".quote-fragments").html(json[beginningQuoteIndex].quote.beginningQuote + " " +
+        json[middleQuoteIndex].quote.middleQuote + " " + json[endQuoteIndex].quote.endQuote);
+        
+        console.log(fullQuote);
+        console.log(author);
+        console.log(fragmentedQuote);
 
         //create dropdown for fullquote or mixed quote selection. Wrap outputs for each instance in functions and call on selection
 
