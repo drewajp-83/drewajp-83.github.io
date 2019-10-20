e51a005180fe25712d0afe8ec3892f45a3e0ac20
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-20 20:15:27 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -28,9 +28,9 @@ $(document).ready(function() {
 
         //create dropdown for 1 - 5 quote selection to generate <li> attributes and loop through on number input
         
-            let dropdownValue = $('#myList option:selected').change(function(){
-                console.log(dropdownValue)
-            });
+            $( "select" ) .change(function () {    
+                document.getElementById("loc").innerHTML="You selected: "+document.getElementById("myList").value;  
+             });  
 
         //event listener to generate quotes
         
