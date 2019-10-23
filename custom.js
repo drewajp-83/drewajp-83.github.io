a13d7b64b031295249d9346c5391248957e5e434
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-10-23 20:00:58 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -7,7 +7,9 @@ $(document).ready(function() {
     let dropdownValue = "";
     $.getJSON("quotes_final.json", function(json) {
         //obtains random array number to generate random full quote
-        const quoteIndex = Math.floor(Math.random() * (json.length));
+        $(function quoteIndex(){
+            Math.floor(Math.random() * (json.length));
+        };
         //obtains random array number for each quote fragment
         const beginningQuoteIndex = Math.floor(Math.random() * (json.length));
         const middleQuoteIndex = Math.floor(Math.random() * (json.length));
