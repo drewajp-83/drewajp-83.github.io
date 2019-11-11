e36cc441039cbe4695c91e5737cbc0894ff8675f
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-11-11 18:34:23 +0000

    Update custom.js

--- custom.js
+++ custom.js
@@ -33,9 +33,9 @@ function handleSelectChange(){
 	//create dropdown for 1 - 5 quote selection to generate <li> attributes and loop through on number input
     $("#myList").change(function printDropdown(){
         quoteCount = $("#myList").val();
-	  for(let j = 0; j < quoteCount; j++){
-	  console.log(j);
-         }
+	  //for(let j = 0; j < quoteCount; j++){
+	  //console.log(j);
+         //}
      });
 }
 
