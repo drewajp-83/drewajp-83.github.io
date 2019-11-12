9be745b627f9af3431ff85f5000a71b8285ae7f6
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-11-12 17:48:42 +0000

    Update custom.js

--- custom.js
+++ custom.js
@@ -85,8 +85,6 @@ function generateQuote(json_data, json_len){
     author = json_data[quoteIndex].author;
     fragmentedQuote = beginningQuote + middleQuote + endQuote
 	
-    //loop through with handleSelectChange value
-	
     //output returned quotes
     if (whichQuote == true) {
         textArea.append('<ul><li>' + fullQuote + '</li><li>' + author + '</li></ul>');
