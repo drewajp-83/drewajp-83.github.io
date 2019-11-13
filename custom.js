595a26bd7262e0383abe4a5bc5a35ff23b3c7dd6
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2019-11-13 14:06:12 +0000

    Update custom.js

--- custom.js
+++ custom.js
@@ -35,8 +35,9 @@ function handleSelectChange(){
     $("#myList").change(function printDropdown(){
         quoteCount = $("#myList").val();
 	  if (quoteCount == 1){
-		quoteCount == 1;
-	  	handleQuoteButtonsClick();
+	  for(let i = 0; i < 1; i++){
+	  	handleQuoteButtonsClick(i);
+	      }
 	  } else {
 	  //loop through quote button handler according to the users input
 	  for(let j = 0; j < quoteCount - 1; j++){
