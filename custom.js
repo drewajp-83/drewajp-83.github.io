56049419ec75659548788d3e428b825f568afd00
drewajp-83 <54987577+drewajp-83@users.noreply.github.com>
2020-07-29 14:39:30 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -43,7 +43,7 @@ function handleAJAX_JSON() {
         console.log(quoteCount);
                 
         //loop through quote button handler according to the users input
-            for (let i = 0; i < quoteCount; i+=) {
+            for (let i = 0; i < quoteCount -1; i++) {
             handleQuoteButtonsClick(i);
             console.log(i);
          }
